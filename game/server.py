668f7a3e3bf9c87db8beb9a7aa23f2f8d4dcfdc8
Yannick <yannickzheng@outlook.fr>
2025-01-01 01:10:16 +0100

    Feat : documentation du serveur

--- game/server.py
+++ game/server.py
@@ -22,23 +22,32 @@ s.listen(5)
 print("En attente de connexion, serveur démarré")
 
 
-#Liste des positions des joueurs
+#position initale des 2 joueurs
 pos = [(0,0),(100,100)]
 def threaded_client(connexion, joueur_actuel):
+    """
+    Fonction permettant de gérer les connexions des clients
+
+    :param connexion: socket de connexion
+    :param joueur_actuel: joueur actuel
+    :return:
+    """
+    #Quand un client se connecte, le serveur lui envoie sa position initiale
     connexion.send(str.encode(tuple_to_str(pos[joueur_actuel])))
     response = ""
     run = True
     while run :
         try:
-            # Réception des données du client
+            #Le serveur reçoit les nouvelles positions du client et les met à jour dans pos
             data = str_to_tuple(connexion.recv(2048).decode())
             pos[joueur_actuel] = data
 
-
+            #Si il n'y a pas de données reçues, on arrête la connexion
             if not data:
                 print("Déconnexion")
                 break
             else:
+                #On envoie la position du joueur actuel à l'autre joueur
                 if joueur_actuel == 1:
                     response = pos[0]
                 else :
@@ -47,7 +56,7 @@ def threaded_client(connexion, joueur_actuel):
                 print("Reçu:", data)
                 print("Envoi:", response)
 
-            connexion.sendall(str.encode(tuple_to_str(response)))
+            connexion.sendall(str.encode(tuple_to_str(response))) #Envoi de la position du joueur actuel à l'autre joueur
         except:
             break
 
@@ -56,9 +65,8 @@ def threaded_client(connexion, joueur_actuel):
 
 joueur_actuel = 0
 while True:
-    #Acceptation de la connexion
-    connexion, address = s.accept()
+    connexion, address = s.accept() #en attente d'une connexion
     print("Connecté à:", address)
-
+    #création d'un nouveau thread pour gérer la connexion de chaque client
     start_new_thread(threaded_client, (connexion, joueur_actuel))
-    joueur_actuel += 1
+    joueur_actuel += 1 #On passe au joueur suivant
