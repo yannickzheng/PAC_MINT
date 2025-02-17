3daa56db8a09f7709bd6bba6913fa9407424260d
ksiksy <alexis.scaglia@gmail.com>
2025-02-17 18:26:39 +0100

    Création d'une Classe RoomManager qui gére les classes a un plus haut niveau (création de salles...)

--- server/rooms.py
+++ server/rooms.py
@@ -1,21 +1,40 @@
 import uuid  #générer des identifiants uniques
 
 """Donne t'on un identifiant pour les players ?"""
+"""Faire en sort qu'un joueur n'est présent que dans une seule salle"""
 
+
+class RoomManager:
+
+    """On instaure un nombre maximum de salles"""
+
+    def __init__(self,room_capacity):
+        self.rooms = {}
+        self.room_capacity = room_capacity
+
+    def create_room(self, room_name, player_capacity = 5):
+        """Création d'une salle'"""
+
+        # Par default, on dit que la capacité des rooms est de 5 joueurs max
+
+        identifier = str(uuid.uuid4())
+        new_room = Room(identifier, player_capacity, room_name)
+        self.rooms[identifier] = new_room
+        return new_room
 
 class Room:
 
-    def __init__(self, identifier, capacity, room_name):
+    def __init__(self, identifier, player_capacity, room_name):
         """
         Création d'une salle'
         """
-        self.capacity = capacity
+        self.player_capacity = player_capacity
         self.players = set() #set pour éviter les doublons
         self.identifier = identifier
         self.room_name = room_name
 
     def is_full(self):
-        if len(self.players) >= self.capacity:
+        if len(self.players) >= self.player_capacity:
             return True
 
     def is_empty(self):
