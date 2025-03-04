77a1d6199504ce33cfddbda650110d54df0c783b
Yannick <yannickzheng@outlook.fr>
2025-03-04 21:34:41 +0100

    variables d'environnement pour accéder au port et à l'adresse ip du serv

--- server/server.py
+++ server/server.py
@@ -3,12 +3,8 @@ from _thread import start_new_thread
 import json
 import os
 
-server_ip = os.getenv("SERVER_IP", "0.0.0.0") #utilise  'server' comme adresse ip par défaut dans docker
-server_port = int(os.getenv("SERVER_PORT", "5555"))
-
-# Adresse IP locale du serveur (ici le serveur est sur la même machine que le client, il doit être modifiable)
-server = server_ip # Adresse IP du serveur (ici localhost)
-port = server_port # Port de communication
+server = os.getenv("SERVER_IP", "0.0.0.0") #utilise  'server' comme adresse ip par défaut dans docker
+port = int(os.getenv("SERVER_PORT", "5555"))
 
 # Création d'un socket pour la communication sur IPV4 en utilisant le protocole TCP
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
