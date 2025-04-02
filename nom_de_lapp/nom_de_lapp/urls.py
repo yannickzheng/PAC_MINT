1b7aa93adc94ff0f563ad3c8f580c4bf2d159ef9
Alban091 <albanr04@gmail.com>
2025-04-02 14:08:43 +0200

    problème de rreference toujours pas réglé

--- nom_de_lapp/nom_de_lapp/urls.py
+++ nom_de_lapp/nom_de_lapp/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from nom_de_lapp.pacmint_app.views import register_player, home  # ✅ Vérifie que .views est bien utilisé
+from pacmint_app.views import register_player, home  # ✅ Vérifie que .views est bien utilisé
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
