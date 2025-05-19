eb13eedcae463a0feacf6b2e99f40bd518ee130d
Alban091 <albanr04@gmail.com>
2025-05-19 09:41:09 +0200

    Problème de unresolved reference réglé et ajout de l'url de la page des scores

--- nom_de_lapp/pacmint_app/urls.py
+++ nom_de_lapp/pacmint_app/urls.py
@@ -3,11 +3,12 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 from django.urls import path
-from .views import CustomLoginView, home, register_player
+from .views import CustomLoginView, home, register_player, score_list,submit_score
 
 urlpatterns = [
     path('', home, name='home'),
     path('inscription/', register_player, name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('api/submit-score/', submit_score, name='submit_score'),
+    path('scores/', score_list, name='score_list'),
 ]
