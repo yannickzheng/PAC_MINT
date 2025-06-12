e511b3dc752947f196143e0f0e5bea58b16b24b2
yannick <yannick.zheng@telecom-sudparis.eu>
2025-06-13 00:15:44 +0200

    changement d'import

--- server/server.py
+++ server/server.py
@@ -2,7 +2,7 @@ import socket
 import time
 import sys
 from _thread import start_new_thread
-from server.rooms import RoomManager
+from rooms import RoomManager
 from common.protocols import Protocols
 
 import threading
