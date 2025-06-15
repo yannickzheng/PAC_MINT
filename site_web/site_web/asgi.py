88c82db42cf8b872db60769461e9aecf353ff8a5
yannick <yannick.zheng@telecom-sudparis.eu>
2025-06-15 21:00:53 +0200

    changement de "nom_de_lapp" vers "site_web"

--- site_web/site_web/asgi.py
+++ site_web/site_web/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for nom_de_lapp project.
+ASGI config for site_web project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nom_de_lapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'site_web.settings')
 
 application = get_asgi_application()
--- site_web/site_web/wsgi.py
+++ site_web/site_web/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for nom_de_lapp project.
+WSGI config for site_web project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nom_de_lapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'site_web.settings')
 
 application = get_wsgi_application()
