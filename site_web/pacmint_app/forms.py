55704fbd868ce06c91dfa814dacb5da3034a9fee
yannick <yannick.zheng@telecom-sudparis.eu>
2025-06-15 22:06:34 +0200

    recréation du formulaire

--- site_web/pacmint_app/forms.py
+++ site_web/pacmint_app/forms.py
@@ -2,20 +2,31 @@ from django import forms
 from .models import Player
 
 
+# class PlayerRegistrationForm(forms.ModelForm):
+#     password = forms.CharField(widget=forms.PasswordInput, max_length=50)
+#     confirm_password = forms.CharField(widget=forms.PasswordInput, max_length=50)
+#
+#     class Meta:
+#         model = Player
+#         fields = ['username', 'email']  # Ajouter ici tous les champs que tu veux dans ton formulaire d'inscription
+#
+#     def clean(self):
+#         cleaned_data = super().clean()
+#         password = cleaned_data.get("password")
+#         confirm_password = cleaned_data.get("confirm_password")
+#
+#         # Vérifier que les mots de passe correspondent
+#         if password != confirm_password:
+#             raise forms.ValidationError("Les mots de passe ne correspondent pas.")
+#         return cleaned_data
+
+
 class PlayerRegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput, max_length=50)
-    confirm_password = forms.CharField(widget=forms.PasswordInput, max_length=50)
+    username = forms.CharField(max_length=50, label="Pseudonyme")
+    password = forms.CharField(widget=forms.PasswordInput, max_length=50, label="Mot de passe")
+    confirm_password = forms.CharField(widget=forms.PasswordInput, max_length=50, label="Confirmez le mot de passe")
+    email = forms.EmailField(max_length=100, label="Email")
 
     class Meta:
         model = Player
-        fields = ['username', 'email']  # Ajouter ici tous les champs que tu veux dans ton formulaire d'inscription
-
-    def clean(self):
-        cleaned_data = super().clean()
-        password = cleaned_data.get("password")
-        confirm_password = cleaned_data.get("confirm_password")
-
-        # Vérifier que les mots de passe correspondent
-        if password != confirm_password:
-            raise forms.ValidationError("Les mots de passe ne correspondent pas.")
-        return cleaned_data
+        fields = ['username', 'password','confirm_password','email']
