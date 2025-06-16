de5b17148c5c088c8e646539a8482c00b2e1f5f6
yannick <yannick.zheng@telecom-sudparis.eu>
2025-06-16 14:25:02 +0200

    Ajout du formulaire d'inscription crispy forms

--- site_web/pacmint_app/forms.py
+++ site_web/pacmint_app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Player
-
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit, Layout, Field
 
 # class PlayerRegistrationForm(forms.ModelForm):
 #     password = forms.CharField(widget=forms.PasswordInput, max_length=50)
@@ -30,3 +31,16 @@ class PlayerRegistrationForm(forms.ModelForm):
     class Meta:
         model = Player
         fields = ['username', 'password','confirm_password','email']
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'S\'inscrire', css_class='btn btn-primary'))
+        self.helper.layout = Layout(
+            Field('username', css_class='form-control'),
+            Field('email', css_class='form-control'),
+            Field('password', css_class='form-control'),
+
+        )
