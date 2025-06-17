a50b8221e52c0248cc9b06bc65e71148112c62a5
Alban091 <albanr04@gmail.com>
2025-06-17 15:43:13 +0200

    Changement de settings pour utiliser la page 404 personnalisé

--- nom_de_lapp/nom_de_lapp/settings.py
+++ nom_de_lapp/nom_de_lapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ul=zxejo6+&qwo0*8vt2lqn^yfzhq4ny-ycza4t3fyo#o@wea&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
