a267e0f6335f811bfa39502bfcebf15d7a89e9ba
yzheng1 <yannickzheng@outlook.fr>
2025-06-18 14:32:54 +0200

    Adaptation de la vue register_player pour conformité avec le formulaire.

--- site_web/pacmint_app/views/register_player.py
+++ site_web/pacmint_app/views/register_player.py
@@ -1,34 +1,16 @@
 from django.shortcuts import render, redirect
 from ..forms import PlayerRegistrationForm
 from django.contrib import messages
-from ..models import Player
-from django.contrib.auth.hashers import make_password
-from django.db import IntegrityError
 
 def register_player(request):
     if request.method == 'POST':
         form = PlayerRegistrationForm(request.POST)
-        print("Données POST :", request.POST)  # Débogage : affiche les données envoyées
         if form.is_valid():
-            print("Formulaire valide")  # Débogage
-            username = form.cleaned_data['username']
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
-            confirm_password = form.cleaned_data['confirm_password']
-            if password != confirm_password:
-                messages.error(request, "Les mots de passe ne correspondent pas, veuillez vérifier votre saisie.")
-                return redirect('pacmint_app:register')
-            try:
-                player = Player(username=username, email=email, password=make_password(password))
-                player.save()
-                messages.success(request, "Votre inscription a été réussie ! Vous pouvez maintenant vous connecter.")
-                return redirect('pacmint_app:login')
-            except IntegrityError:
-                messages.error(request, "Ce nom d'utilisateur ou cet email est déjà utilisé.")
-                return redirect('pacmint_app:register')
+            form.save()
+            messages.success(request, "Inscription réussie ! Vous pouvez maintenant vous connecter.")
+            return redirect('pacmint_app:login')
         else:
-            print("Formulaire non valide :", form.errors)  # Débogage : affiche les erreurs
-            messages.error(request, "Erreur dans le formulaire. Veuillez vérifier vos informations.")
+            messages.error(request, "Erreur lors de l'inscription. Vérifiez les informations fournies.")
     else:
         form = PlayerRegistrationForm()
     return render(request, 'register.html', {'formulaire': form})
