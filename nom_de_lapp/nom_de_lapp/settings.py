bdd3a60c78b9806f6b8d2640148c8cdb9d4a2390
Alban091 <albanr04@gmail.com>
2025-06-18 15:10:33 +0200

    Changement de settings pour collectstatic et faire apparaitre le logo et la page 404 custom lorsque DEBUG = False, maintenant il faut que je vois pour le favicon

--- nom_de_lapp/nom_de_lapp/settings.py
+++ nom_de_lapp/nom_de_lapp/settings.py
@@ -122,7 +122,7 @@ import os
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'pacmint_app/static')
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Pour collectstatic
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
