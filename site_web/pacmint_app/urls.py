d76d2dbefc4bb5cd04240ec4bf6d5d1261edf9bd
Alban091 <albanr04@gmail.com>
2025-06-30 15:40:47 +0200

    Réglage problème d'import de submit_game_result

--- site_web/pacmint_app/urls.py
+++ site_web/pacmint_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.conf import settings
 from . import views
+from .views.api import submit_game_result
 
 app_name = 'pacmint_app'
 
@@ -13,7 +14,7 @@ urlpatterns = [
     path('login/', views.login_player, name='login'),
     path('profile/', views.profile_view, name='profile'),
     path('logout/', views.logout_view, name='logout'),
-
+    path('api/submit-result/', submit_game_result, name='submit_result'),
 ]
 
 # URLs de test seulement en mode DEBUG
