e70f942d6a071b0e311a21a8f805422f9e1217ab
Alban091 <albanr04@gmail.com>
2025-07-07 11:05:32 +0200

    ajout de la vue du classement global

--- site_web/pacmint_app/views/__init__.py
+++ site_web/pacmint_app/views/__init__.py
@@ -6,3 +6,4 @@ from .welcome import welcome_player
 from .login_player import login_player
 from .profile_view import profile_view
 from .logout_player import logout_view
+from .leaderboard_view import leaderboard_view
