7b71a9d063045b4ae5b693b069d2063ea040d52e
Alban091 <albanr04@gmail.com>
2025-07-11 17:24:04 +0200

    Migration : suppression du modèle Score, suppression des anciens scores que j'avais mis en place et qui ne servent plus

--- site_web/pacmint_app/views/score_list.py
+++
@@ -1,7 +0,0 @@
-from ..models import Score
-from django.shortcuts import render
-from django.contrib.auth.models import User
-
-def score_list(request):
-    scores = Score.objects.all().order_by('-value')[:20]  # Top 20
-    return render(request, 'scores.html', {'scores': scores})
