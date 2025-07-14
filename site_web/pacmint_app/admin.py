67907217697f8b202e2f1bbd24eaae7e0c4fa2a5
Alban091 <albanr04@gmail.com>
2025-07-14 12:16:47 +0200

    Register all models in Django admin

--- site_web/pacmint_app/admin.py
+++ site_web/pacmint_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import (
+    Player, GameResult, Friend,
+    Room, Room_Players, Game,
+    Table_Score, Map
+)
 
-# Register your models here.
+admin.site.register(Player)
+admin.site.register(GameResult)
+admin.site.register(Friend)
+admin.site.register(Room)
+admin.site.register(Room_Players)
+admin.site.register(Game)
+admin.site.register(Table_Score)
+admin.site.register(Map)
+
